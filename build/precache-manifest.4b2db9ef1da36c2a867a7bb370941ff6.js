65f04d5b73f7bf5228f1b127acb5befa41700386
Michael Guo <ismichaelguo@gmail.com>
2020-07-07 17:59:21 +1000

    upload my weather app

---
+++ build/precache-manifest.4b2db9ef1da36c2a867a7bb370941ff6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46aaf0384a9b63cfc71d414fbdc6d268",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b732c6e0ee218f275f43",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "13f7c16b29f4e0045d53",
+    "url": "/static/css/main.4f195e38.chunk.css"
+  },
+  {
+    "revision": "b732c6e0ee218f275f43",
+    "url": "/static/js/2.f6a60e1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f6a60e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f7c16b29f4e0045d53",
+    "url": "/static/js/main.15acb2e9.chunk.js"
+  },
+  {
+    "revision": "f062ecea296df51d7867",
+    "url": "/static/js/runtime-main.d817765f.js"
+  },
+  {
+    "revision": "9129d938494ad53da00fff99ada223d3",
+    "url": "/static/media/cold.9129d938.svg"
+  },
+  {
+    "revision": "126f15b7c2ba3e68d5a2e7020c888886",
+    "url": "/static/media/high-temperature.126f15b7.svg"
+  },
+  {
+    "revision": "e4431485c95f3c961503afa90fa593fe",
+    "url": "/static/media/water.e4431485.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,144 @@
+import React from "react";
+import "./app.scss";
+import { FiSearch } from "react-icons/fi";
+import CityCardContainer from "./component/city-card-container/CityCardContainer";
+import WeatherForecastBoard from "./component/weather-forecase-board/WeatherForecastBoard";
+import CurrentWeatherBoard from "./component/current-weather-board/CurrentWeatherBoard";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchCity: [],
+      photo: [],
+      imageId: 0,
+      foreCastWeather: [],
+      currentWeather: [],
+    };
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    let cityTest = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
+    let isCity = cityTest.test(e.target.elements[0].value);
+
+    let city = e.target.elements[0].value || "Sydney";
+    //splash photo api
+    const Access_key = "BHJyBbJMrYRBqHzYuivvgBVspMShqGua5e0W1BvBkrA";
+    const photo_api = await fetch(
+      `https://api.unsplash.com/search/photos/?client_id=${Access_key}&query=${city}&orientation=landscape`
+    );
+    const response_photo = await photo_api.json();
+
+    const nameError = document.getElementById("nameError");
+    const limitError = document.getElementById("limitError");
+
+    let cityItem;
+    //store input cities into this.state.searchCity only when the photo data exited in splash api
+    if (response_photo.results.length > 0) {
+      cityItem = {
+        id: new Date(),
+        city: city,
+        cityImage: response_photo.results[0].urls.small,
+      };
+      let cityNumber = this.state.searchCity.length;
+      //the maximum city number is 4
+      if (cityNumber < 4) {
+        this.setState((prevState) => ({
+          searchCity: prevState.searchCity.concat(cityItem),
+        }));
+      } else if (isCity === true && cityNumber > 3) {
+        limitError.style.height = "2rem";
+        setTimeout(() => (limitError.style.height = "0rem"), 4000);
+      } else if (isCity === false) {
+        nameError.style.height = "2rem";
+        setTimeout(() => (nameError.style.height = "0rem"), 4000);
+      }
+    } else {
+      nameError.style.height = "2rem";
+      setTimeout(() => (nameError.style.height = "0rem"), 4000);
+    }
+  };
+
+  getWeatherData = async (e) => {
+    // forecast weather api
+    const API_KEY = "45ba571f29c54202a86d5d62ca83141a";
+    const city = e.target.id;
+    const FroeCastWeatherData = await fetch(
+      `http://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${API_KEY}`
+    );
+    const ForeCastResponse = await FroeCastWeatherData.json();
+    this.setState({
+      foreCastWeather: ForeCastResponse,
+    });
+
+    const selectedCity = document.getElementById(city);
+    selectedCity.style.transform = "scale(1.1)";
+
+    //current weather api
+    const CurrentWeatherData = await fetch(
+      `https://api.weatherbit.io/v2.0/forecast/hourly?city=${city}&key=${API_KEY}&hours=48`
+    );
+    const CurrentResponse = await CurrentWeatherData.json();
+    this.setState({
+      currentWeather: CurrentResponse,
+    });
+  };
+
+  deleteCard = (cardIndex) => {
+    let prevCities = new Set(this.state.searchCity);
+    //return deleted cities
+    let updatedCities = new Set(this.state.searchCity.splice(cardIndex, 1));
+    //get the different set with prev cities,which are the cities want to remain
+    let filterCities = Array.from(
+      new Set([...prevCities].filter((x) => !updatedCities.has(x)))
+    );
+    this.setState({
+      searchCity: filterCities,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="app__container">
+          <section className="app__container__left">
+            {/* search bar */}
+            <FiSearch className="search-icon" />
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="Search new place"></input>
+              <button onClick={this.getData}>Search</button>
+            </form>
+            <div className="app__container__error" id="nameError">
+              It is not a Valid City Name!
+            </div>
+            <div className="app__container__error" id="limitError">
+              You only can add 4 cities!
+            </div>
+            {/* title */}
+            <h4>
+              Weather <span>Forecast</span>
+            </h4>
+            {/* city cards */}
+            <CityCardContainer
+              cities={this.state.searchCity}
+              imageId={this.state.imageId}
+              photo={this.state.photo}
+              getWeatherData={this.getWeatherData}
+              selectedCity={this.state.foreCastWeather}
+              deleteCard={this.deleteCard}
+            />
+            {/* Weather Forecast Board */}
+            <WeatherForecastBoard weather={this.state.foreCastWeather} />
+          </section>
+          <section className="app__container__right">
+            {/* Current Weather Board */}
+            <CurrentWeatherBoard weather={this.state.currentWeather} />
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
